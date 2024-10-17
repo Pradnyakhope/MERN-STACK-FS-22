8003be5aeb575ed4fb14b196b7a2198343df2bd1
Delta Student <deltastudent@apnacollege.in>
2024-10-17 15:56:34 +0530

    Dom-project is completed

---
+++ script.js
@@ -0,0 +1,29 @@
+// let h1 = document.getElementsByTagName("h1");
+// console.log(h1);
+// let convertArray = Array.from(h1);
+// console.log(convertArray);
+
+
+// let h1 = document.getElementById("head");
+// console.log(h1);
+
+// let h1 = document.getElementsByClassName("head");
+// console.log(h1);
+
+// let h1 = document.querySelectorAll(".head");
+// console.log(h1);
+
+let div = document.querySelector("#myDiv");
+console.log(div);
+
+div.style.width = "300px";
+div.style.height = "200px";
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.color = "blue";
+
+
+
